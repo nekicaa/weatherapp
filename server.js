10a6fd58b5ff9a8ede3b45c23eeeefa4e10f5535
nekicaa <nevenakrstic98@gmail.com>
2020-01-26 16:36:26 +0100

    greska

--- server.js
+++ server.js
@@ -19,13 +19,13 @@ let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric
 
 request(url, function (err, response, body) {
   if(err){
-    res.render('index', {weather: null, error: 'Error, please try again'});
+    res.render('index', {weather: null, error: 'Greška, molimo Vas pokušajte ponovo'});
   } else {
     let weather = JSON.parse(body)
     if(weather.main == undefined){
-      res.render('index', {weather: null, error: 'Error, please try again'});
+      res.render('index', {weather: null, error: 'Greška, molimo Vas pokušajte ponovo'});
     }else{
-      let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+      let weatherText = `Sada je ${weather.main.temp} stepeni u ${weather.name}!`;
         res.render('index', {weather: weatherText, error: null});
     }
   }
@@ -33,5 +33,5 @@ request(url, function (err, response, body) {
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Weather app listening on port 3000!')
 })
